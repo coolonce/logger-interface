81836366a6ee26c1cae1eaabe5646cb4f3181058
Vadim <coolonce@yandex.ru>
2019-08-08 12:52:42 +0300

    ooops

--- src/LoggerInterface.php
+++ src/LoggerInterface.php
@@ -9,7 +9,7 @@
 namespace Logger;
 
 
-public interface LoggerInterface
+interface LoggerInterface
 {
     public function Send(int $user_id = 0, int $act_id = 0, int $service_id = 0, string $data = '', string $function_name = '');
 
